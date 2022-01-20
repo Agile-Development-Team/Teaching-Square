f3e3afb703735ac08ef0ef7ea9e8b2be040ff5ad
unknown <2826785351@qq.com>
2022-01-20 20:52:14 +0800

    Add my impression

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="nav">
+      <router-link to="/">主页</router-link> |
+      <router-link to="/login">登录</router-link> |
+      <router-link to="/register">注册</router-link> |
+      <router-link to="/student">学生</router-link> |
+    </div>
+    <router-view/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
   }
 }
 </script>
@@ -23,6 +26,18 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
+
+/* // #nav {
+//   padding: 30px;
+
+//   a {
+//     font-weight: bold;
+//     color: #2c3e50;
+
+//     &.router-link-exact-active {
+//       color: #42b983;
+//     }
+//   }
+// } */
 </style>
---
+++ web/src/views/Home.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="home">
+    <p>Home</p>
+    <el-row>
+      <el-button @click="hanldeTeacher" type="primary">我是教师</el-button>
+      <el-button @click="hanldeStudent" type="success">我是学生</el-button>
+    </el-row>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'Home',
+  components: {
+  },
+  data () {
+    return {
+    }
+  },
+  methods: {
+    hanldeTeacher () {
+      this.$router.replace('/login')
+    },
+    hanldeStudent () {
+      this.$router.replace('/login')
+    }
+  }
+}
+</script>
