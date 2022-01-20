55ed374405f472e6c83aa213021470b777011ad3
Carol9s <lilyzhangyiyang@126.com>
2022-01-20 21:52:59 +0800

    StudentInfo

---
+++ backend/src/main/java/com/example/ts/util/StudentInfo.java
@@ -0,0 +1,11 @@
+package com.example.ts.util;
+
+/**
+ * @Author Carol9s
+ * @Classname StudentInfo
+ * @Date 2022/1/20 21:50
+ */
+public class StudentInfo {
+    private String name;
+    private String number;
+}
