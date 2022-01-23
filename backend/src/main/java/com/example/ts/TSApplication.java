82169e96a34b6b0dcbe7c3fd35275b5c30fb0710
Carol9s <lilyzhangyiyang@126.com>
2022-01-23 22:04:53 +0800

    Init Update

---
+++ backend/src/main/java/com/example/ts/TSApplication.java
@@ -0,0 +1,16 @@
+package com.example.ts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author Carol9s
+ * @Classname TSApplication
+ * @Date 2022/1/23 19:29
+ */
+@SpringBootApplication
+public class TSApplication {
+    public static void main(String[]args){
+        SpringApplication.run(TSApplication.class, args);
+    }
+}
